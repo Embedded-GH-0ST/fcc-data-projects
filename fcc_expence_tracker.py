789cc2d9650d66955709dda99e31df3c6e004227
Embedded-GH-0ST <chanceybeneski@gmail.com>
2026-01-02 03:07:57 -0500

    added notes

--- fcc_expence_tracker.py
+++ fcc_expence_tracker.py
@@ -1,16 +1,20 @@
+# add expense
 def add_expense(expenses, amount, category):
     expenses.append({"amount": amount, "category": category})
 
 
+# print expenses
 def print_expenses(expenses):
     for expense in expenses:
         print(f"Amount: {expense['amount']}, Category: {expense['category']}")
 
 
+# total expenses
 def total_epenses(expenses):
     return sum(map(lambda expense: expense["amount"], expenses))
 
 
+# filter expenses by category
 def filter_expenses_by_category(expenses, category):
     return lambda expense: expense["category"] == category, expen
 
