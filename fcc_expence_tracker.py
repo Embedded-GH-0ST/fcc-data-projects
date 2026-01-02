cf2c67baeff0f628f0a81da37bbd49654843e445
Embedded-GH-0ST <chanceybeneski@gmail.com>
2026-01-02 03:53:14 -0500

    added main function

--- fcc_expence_tracker.py
+++ fcc_expence_tracker.py
@@ -19,4 +19,13 @@ def filter_expenses_by_category(expenses, category):
     return lambda expense: expense["category"] == category, expen
 
 
-expenses = []
+# main function
+def main():
+    expenses = []
+    while True:
+        print("\nExpense Tracker")
+        print("1. Add an expense")
+        print("2. List all expenses")
+        print("3. Show total expenses")
+        print("4. Filter expenses by category")
+        print("5. Exit")
